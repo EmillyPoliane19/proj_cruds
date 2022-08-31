e82c28637448323b3758297e1df8f302bfd1c86d
GUSTAVO HENRIQUE DOS SANTOS BULHOES <20191194010035@ifrn.local>
2022-08-31 08:55:43 -0300

    atualização

--- core/models.py
+++ core/models.py
@@ -4,7 +4,7 @@ class Prof(models.Model):
    nome = models.CharField('Nome', max_length=100)
    data = models.DateField('Data de nascimento')
    materia = models.CharField('Matéria', max_length=100)
-   image = models.ImageField(upload_to='uploadprof/', default="Some String")
+   image = models.ImageField(blank=True, upload_to='images')
 
 class Curso(models.Model):
    nome = models.CharField('Nome', max_length=100)
--- proj_cruds/settings.py
+++ proj_cruds/settings.py
@@ -120,8 +120,8 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 import os
-MEDIA_URL = '/templates/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'templates')
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
